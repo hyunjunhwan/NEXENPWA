3e8cdca11a1c7f23bd0bc0be5d811e9ce6f0a8fb
hyunjunhwan <94382710+hyunjunhwan@users.noreply.github.com>
2022-05-26 16:42:58 +0900

    Initial commit

--- pages/_document.js
+++ pages/_document.js
@@ -8,6 +8,7 @@ class MyDocument extends Document {
           <link rel="manifest" href="/manifest.json" />
           <link rel="apple-touch-icon" href="/icon.png"></link>
           <meta name="theme-color" content="#fff" />
+          <meta name="mobile-web-app-capable" content="yes" />
         </Head>
         <body>
           <Main />
